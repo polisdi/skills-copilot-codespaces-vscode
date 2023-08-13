9ab9ea5fe99741660de77055564ece4c6c8c9490
polisdi <142172989+polisdi@users.noreply.github.com>
2023-08-13 18:17:26 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember()
+{
+    var member= document.getElementById("member");
+    member.style.display="block";
+    var skills= document.getElementById("skills");
+    skills.style.display="none";
+    var about= document.getElementById("about");
+    about.style.display="none";
+    var contact= document.getElementById("contact");
+    contact.style.display="none";
+}
